56c2c813392e548aee3039eb8fb464b6b74a703e
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-11-03 17:53:00 +0100

    Creazione rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import ChiSono from "./pages/ChiSono"
+import Contatti from "./pages/Contatti"
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="ChiSono" element={<ChiSono />} />
+          <Route path="Contatti" element={<Contatti />} />
+        </Route>
+      </Routes>
+    </Router>
     </>
   )
 }
